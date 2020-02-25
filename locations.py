86f5d7d5ad01c475c8ba16f676ab84eef13bb7cf
SlavkoPrytula <slavko.prytula@gmail.com>
2020-02-25 21:34:08 +0200

    Update locations.py

--- locations.py
+++ locations.py
@@ -6,8 +6,8 @@ def get_locations(user_name):
 	Gets all friends of a user using Twitter API keys.
 	And returns dictionary with names as keys and locations as value.
 	"""
-    auth = tweepy.auth.OAuthHandler("473GeY7AOJEYhjb3V3bQJRVXd", "OgRJWHJjd66o6uGA65Vx7N0ebSbe4axMKNsC36j0iwg64H9vV3")
-    auth.set_access_token("851332427343593472-Nql8TgigLr6LH0BSJVYW85SSTE3ATlu", "WnY2tUnh7u1HhkLhe18OATMzUlJaZtYpiLXg91ayiRORr")
+    auth = tweepy.auth.OAuthHandler("API key", "API secret key")
+    auth.set_access_token("Access token", "Access token secret")
     api = tweepy.API(auth)
     users = tweepy.Cursor(api.friends, screen_name=user_name).items()
 
